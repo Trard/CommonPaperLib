8bda240fe4cbd782fedcf66eb1dec968fb593445
trard <trardone@gmail.com>
2024-07-06 17:07:52 +0300

    teleportAndSpectate

--- src/main/kotlin/utils/teleportAndSpectate.kt
+++ src/main/kotlin/utils/teleportAndSpectate.kt
@@ -1,16 +1,21 @@
 package com.github.trard.utils
 
 import com.github.trard.Scheduler
+import org.bukkit.GameMode
 import org.bukkit.entity.Player
 
 fun teleportAndSpectate(scheduler: Scheduler, spectator: Player, target: Player) {
+    spectator.gameMode = GameMode.SPECTATOR
+
     scheduler.runAtEntityLater(target, 10, null) {
         spectator.teleportAsync(target.location)
+        spectator.gameMode = GameMode.SPECTATOR
         spectator.spectatorTarget = null
     }
 
     scheduler.runAtEntityLater(target, 60, null) {
         spectator.teleportAsync(target.location)
+        spectator.gameMode = GameMode.SPECTATOR
         spectator.spectatorTarget = target
     }
 }
