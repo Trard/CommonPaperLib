411d5b82bb1fee58b7a1aaa612a80fd7c3057891
trard <trardone@gmail.com>
2024-07-10 23:47:08 +0300

    37

--- src/main/kotlin/utils/teleportAndSpectate.kt
+++ src/main/kotlin/utils/teleportAndSpectate.kt
@@ -13,12 +13,6 @@ fun teleportAndSpectate(scheduler: Scheduler, spectator: Player, target: Player)
         spectator.spectatorTarget = null
     }
 
-    // elytra speed at 52 degrees is ~3.365 blocks/tick (https://minecraft.fandom.com/wiki/Transportation 67.3 m/s)
-    // average minecraft view distance is 8 chunks so 8*16=128 blocks
-    // round(128 / 3.365) = 38 ticks enough to get out from view distance
-    // 37 because 1 tick for assurance
-    //
-    // it's possible to break this limit faster, but it's probably never going to happen
     scheduler.runAtEntityLater(target, 37, null) {
         spectator.teleportAsync(target.location)
         spectator.gameMode = GameMode.SPECTATOR
